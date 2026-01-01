15f10c7b29d3d90624f3e4912ef181921a6f2aee
LSQUARE14 <trdhoanglong05@gmail.com>
2026-01-02 00:28:54 +0700

    for final

--- ai/app/services/mcp_server.py
+++ ai/app/services/mcp_server.py
@@ -6,21 +6,21 @@ mcp = FastMCP("rule_writer")
 
 @mcp.tool(name="write_to_file", description="Write provided ModSecurity rules into rule file (append). Argument: content: str, file_name: str")
 def write_to_file(content: str, file_name: str) -> str:
-    with open(file_name, "a") as file:
+    with open("/rules/" + "custom.conf", "a") as file:
         file.write(content)
     return f"Content written to {file_name}"
 
 @mcp.tool(name="rewrite_rule_file", description="Rewrite the rule file with provided content. Argument: content: str, file_name: str")
 def rewrite_rule_file(content: str, file_name: str) -> str:
-    with open(file_name, "w") as file:
+    with open("/rules/" + "custom.conf", "w") as file:
         file.write(content)
     return f"Rewritten {file_name}"
 
-@mcp.tool(name="read_rule_file", description="Read the current rules.txt. Returns empty string if file not found or file empty. Files' names are sqli.conf, xss.conf, path_traversal.conf, cmdi.conf,  Argument: file_name: str")
+@mcp.tool(name="read_rule_file", description="Read the current rules.txt. Returns empty string if file not found or file empty. Files' names are sqli.conf, xss.conf, path_traversal.conf, cmdi.conf, custom.conf.  Argument: file_name: str")
 def read_rule_file(file_name: str) -> str:
     try:
-        with open(file_name, "r") as file:
-            return file.read()
+        with open("/rules/" + file_name, "r") as file:
+            return file_name + "\n" + file.read()
     except FileNotFoundError:
         return ""
 
@@ -30,3 +30,6 @@ def _run():
 def start_mcp_server():
     t = threading.Thread(target=_run, daemon=True)
     t.start()
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
